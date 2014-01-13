c96bbe3f13f24f4e2705e1b8c820ee7fb85fe7d4
Luis Aguilar <luismanuel.aguilar@altran.com>
2014-01-13 16:29:44 +0000

    Added command line arguments processment

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Git is not that HHHard!!!");
+		for (int i = 0; i < args.length ; i++)
+			System.out.println(args[i]);
 
 	}
 
